3f1ba0c957fce6c7181a9ca1282b2922fb14107a
indominuShot <luang193@gmail.com>
2020-06-17 20:33:24 -0300

    configurações de lint

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({Message : "Hello World!"});
-}); 
+  res.json({ Message: 'Hello World!' });
+});
 
 app.listen(3333);
